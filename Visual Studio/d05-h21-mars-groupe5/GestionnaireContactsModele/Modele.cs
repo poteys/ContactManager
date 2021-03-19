5ff68db59b94c0655fcf1fd4a94bbf7ee8e028ba
Juliana Elles Lopez <jelleslo@isi-montreal.com>
2021-03-19 12:43:34 -0400

    ajout de la class contact

--- Visual Studio/d05-h21-mars-groupe5/GestionnaireContactsModele/Modele.cs
+++ Visual Studio/d05-h21-mars-groupe5/GestionnaireContactsModele/Modele.cs
@@ -2,10 +2,15 @@
 
 namespace GestionnaireContactsModele
 {
-    public class Modele
+    public class Contact
     {
 
-
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public int? Age { get; set; }
+        public long Telephone { get; set; }
+        public string Ville { get; set; }
 
     }
 }
